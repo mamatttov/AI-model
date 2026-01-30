0706cce5e6b05b0f817e36b8ed1066b91627b31a
Kairatbek Mamatov <161651285+mamatttov@users.noreply.github.com>
2026-01-31 00:54:26 +0600

    feat: add Information and Transcribing components, enhance App layout and loading animations

--- AI-model/src/App.jsx
+++ AI-model/src/App.jsx
@@ -2,11 +2,15 @@ import FileDisplay from "./components/FileDisplay";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import { useState, useEffect, useRef } from "react";
-
+import Information from "./components/Information";
+import Transcribing from "./components/Transcirbing";
+import "./index.css";
 function App() {
   const [file, setFile] = useState(null);
   const [audioStream, setAudioStream] = useState(null);
   const isAudioAvailable = file || audioStream;
+  const [output, setOutput] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   function handleAudioReset() {
     setFile(null);
@@ -19,11 +23,15 @@ function App() {
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
         <Header />
-        {isAudioAvailable ? (
+        {output ? (
+          <Information output={output} finished={finished} />
+        ) : loading ? (
+          <Transcribing />
+        ) : isAudioAvailable ? (
           <FileDisplay
+            handleAudioReset={handleAudioReset}
             file={file}
             audioStream={audioStream}
-            handleAudioReset={handleAudioReset}
           />
         ) : (
           <HomePage setFile={setFile} setAudioStream={setAudioStream} />
--- AI-model/src/components/Transcirbing.jsx
+++ AI-model/src/components/Transcirbing.jsx
@@ -2,23 +2,25 @@ import React from "react";
 
 export default function Transcribing(props) {
   const { downloading } = props;
+
   return (
-    <div className="flex flex-col items-center justify-center gap-10 md:gap-14 py-24">
+    <div className="flex items-center flex-1 flex-col justify-center gap-10 md:gap-14 text-center pb-24 p-4">
       <div className="flex flex-col gap-2 sm:gap-4">
-        <h1 className="font-semibold text-5xl sm:text-6xl md:text-7xl text-blue-400 bold ">
-          Transcribing
+        <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
+          <span className="text-blue-400 bold">Transcribing</span>
         </h1>
         <p>
-          {!downloading ? "warming up the cylinders" : "core cylinders engaged"}
+          {!downloading ? "warming up cylinders" : "core cylinders engaged"}
         </p>
       </div>
-      <div className="flex flex-col gap-2 sm:gap-4 max-w-[500px] mx-auto w-full">
-        {[0, 1, 2, 3].map((val) => {
+      <div className="flex flex-col gap-2 sm:gap-3 max-w-[400px] mx-auto w-full">
+        {[0, 1, 2].map((val) => {
           return (
             <div
+              key={val}
               className={
-                "rouneded-full h-2 sm:h3 bg-slate-400 loading" +
-                ` loading${val}`
+                "rounded-full h-2 sm:h-3 bg-slate-400 loading " +
+                `loading${val}`
               }
             ></div>
           );
